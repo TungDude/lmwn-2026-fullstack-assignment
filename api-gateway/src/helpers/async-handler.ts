831f23fd87f8c98295ca75a1ba71a3c0f9d4fcd3
TungDude <krittapop.kms@gmail.com>
2026-02-03 18:46:44 +0700

    fix(ci): lint

--- api-gateway/src/helpers/async-handler.ts
+++ api-gateway/src/helpers/async-handler.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<any>;
+type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>;
 
 export const asyncHandler = (fn: AsyncHandler) => {
     return (req: Request, res: Response, next: NextFunction) => {
