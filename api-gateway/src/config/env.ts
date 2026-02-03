e5b47b42c0e4db229448137381eea19b3a79a68e
TungDude <krittapop.kms@gmail.com>
2026-02-03 18:38:51 +0700

    feat(gateway): helpers

---
+++ api-gateway/src/config/env.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+
+export const config = {
+    port: process.env.PORT || 3001,
+    env: process.env.ENV || 'development',
+    corsOptions: {
+        origin: process.env.CORS_ALLOWED_ORIGINS?.split(',') || ['http://localhost:3000'],
+    },
+    guideServiceUrl: process.env.GUIDE_SERVICE_URL || 'http://localhost:8888',
+    restaurantServiceUrl: process.env.RESTAURANT_SERVICE_URL || 'http://localhost:9999',
+}
---
+++ api-gateway/src/helpers/send-response.ts
@@ -0,0 +1,13 @@
+import { Response } from "express";
+import type { ApiResponse } from "@shared/packages";
+
+export const sendResponse = <T>(res: Response, status: number, data: T, message: string | null = null) => {
+    const response: ApiResponse<T> = {
+        success: status >= 200 && status < 300,
+        message,
+        data,
+        timestamp: new Date().toISOString()
+    };
+
+    return res.status(status).json(response);
+};
