1d52fd693b9e3f57f03201d8f224565c294aef1b
TungDude <krittapop.kms@gmail.com>
2026-02-05 02:08:04 +0700

    feat(web): layout scroll listener with back to top

--- web/src/components/layout/AppLayout.tsx
+++ web/src/components/layout/AppLayout.tsx
@@ -1,8 +1,41 @@
 import { Outlet } from "react-router-dom";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Fab, Typography } from "@mui/material";
 import Header from "@/components/ui/organisms/Header";
+import { useState, useEffect, useRef } from "react";
+import { ChevronUp } from "lucide-react";
 
 export default function AppLayout() {
+    const [showFab, setShowFab] = useState(false);
+    const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (scrollContainerRef.current) {
+                if (scrollContainerRef.current.scrollTop > 300) {
+                    setShowFab(true);
+                } else {
+                    setShowFab(false);
+                }
+            }
+        };
+
+        const scrollContainer = scrollContainerRef.current;
+        scrollContainer?.addEventListener("scroll", handleScroll);
+
+        return () => {
+            scrollContainer?.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        }
+    };
+
     return (
         <Stack
             direction="column"
@@ -12,14 +45,33 @@ export default function AppLayout() {
         >
             <Header />
             <Box
+                ref={scrollContainerRef}
                 flexGrow={1}
                 padding={4}
+                paddingBottom={6}
                 overflow="auto"
                 className="custom-scrollbar"
             >
                 <Outlet />
             </Box>
+            {showFab && (
+                <Fab
+                    variant="extended"
+                    size="small"
+                    onClick={scrollToTop}
+                    style={{
+                        position: "fixed",
+                        bottom: 16,
+                        right: 16,
+                        paddingRight: 16,
+                    }}
+                >
+                    <Stack direction="row" spacing={1} alignItems="center">
+                        <ChevronUp size={20} />
+                        <Typography variant="caption">Back to Top</Typography>
+                    </Stack>
+                </Fab>
+            )}
         </Stack>
-
-    )
+    );
 }
