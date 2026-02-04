7c41290fe1f84e3c4d04a7a20ff5528945f9b1ef
TungDude <krittapop.kms@gmail.com>
2026-02-05 03:15:16 +0700

    fix(asset): logo name

--- web/src/components/ui/organisms/Header.tsx
+++ web/src/components/ui/organisms/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header className="sticky bg-white top-0 z-10 flex items-center justify-between px-8 py-2 border-b border-gray-300">
             <img
-                src="/fav.svg"
+                src="/lmwn-logo.svg"
                 alt="LMWN"
                 style={{
                     width: 48,
